15ec4492dd5d3c812343f2f27888eaf72d89115d
Nico <nicolas.schomberg@gmail.com>
2017-03-17 15:11:27 -0700

    Refactor
    * Logic Reorganization
    
    * Unit Tests
    
    * fat arrow
    
    * Unit tests
    
    * Unit tests
    
    * Unit tests
    
    * Unit Test
    
    * Travis
    
    * Unit tests
    
    * Integrated Tanuki
    
    * Unit test overhaul
    
    * Adding linter to test

--- index.js
+++ index.js
@@ -4,28 +4,27 @@ let express = require('express');
 let app = express();
 
 let PingBot = require('./lib/ping-bot');
-let db = require('./lib/ping-bot/db');
 
 
 let bot = new PingBot();
 
 bot.init()
-	.then(() => {
-		app.set('port', (process.env.PORT || 5000));
+  .then(() => {
+    app.set('port', (process.env.PORT || 5000));
 
-		app.use(express.static(__dirname + '/public'));
+    app.use(express.static(__dirname + '/public'));
 
-		// views is directory for all template files
-		app.set('views', __dirname + '/views');
-		app.set('view engine', 'ejs');
+    // views is directory for all template files
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'ejs');
 
-		app.get('/', function(request, response) {
-			response.render('pages/index', {
-				bot: bot,
-			});
-		});
+    app.get('/', function(request, response) {
+      response.render('pages/index', {
+        bot: bot,
+      });
+    });
 
-		app.listen(app.get('port'), function() {
-			console.log('Node app is running on port', app.get('port'));
-		});
-	});
+    app.listen(app.get('port'), function() {
+      console.log('Node app is running on port', app.get('port'));
+    });
+  });
